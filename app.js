dc34561aba904b1f3dc879f4bfcc259e7882004a
Russell <russelldev@protonmail.com>
2020-12-26 18:33:19 +0000

    Add seperate func for qselector

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const jokes = document.querySelector('#jokes');
 const btn = document.querySelector('.btn');
-btn.addEventListener('click', ()=>{
-    getDadJoke();
-});
 
-const getDadJoke = async ()=>{
+const addNewJoke = async()=>{
+    const jokeText = await getDadJoke();
+    console.log(jokeText);
+    const newLI = document.createElement('li');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+btn.addEventListener('click', addNewJoke);
+
+const getDadJoke = async()=>{
     const config = {headers: {Accept: 'application/json'}};
     const res = await axios.get('https://icanhazdadjoke.com/', config);
-    const newLI = document.createElement('li');
-    newLI.append(res.data.joke);
-    jokes.append(newLI);
-
-
+    return res.data.joke;
 }
+
